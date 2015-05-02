d86e501d08d913aa709de4dc15ea5f94af7d40f9
BelfodilAimene <ba_belfodil@esi.dz>
2015-05-02 13:16:52 +0100

    hachage mdp

--- app/src/main/java/smartcity/begrouped/utils/GlobalMethodes.java
+++ app/src/main/java/smartcity/begrouped/utils/GlobalMethodes.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by Anes on 01/05/2015.
@@ -53,5 +55,30 @@ public class GlobalMethodes {
             Log.e("Buffer Error", "Error converting result " + e.toString());
         }
         return chaine;
+    }
+
+    public static final String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
