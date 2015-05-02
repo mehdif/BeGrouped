549ecb1e9b150aa658503cc538133fdc8585abd4
Anes <bendimeradanes@gmail.com>
2015-05-02 11:38:16 +0200

    Commited

--- app/src/main/java/smartcity/begrouped/controllers/POIManager.java
+++ app/src/main/java/smartcity/begrouped/controllers/POIManager.java
@@ -1,23 +1,174 @@
 package smartcity.begrouped.controllers;
 
+import android.util.Log;
+
 import com.google.android.gms.maps.model.Marker;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.LinkedList;
 
+import smartcity.begrouped.model.Date;
+import smartcity.begrouped.model.Location;
 import smartcity.begrouped.model.POI;
+import smartcity.begrouped.model.Temps;
+import smartcity.begrouped.model.User;
+import smartcity.begrouped.utils.AllUrls;
+import smartcity.begrouped.utils.Constants;
+import smartcity.begrouped.utils.GlobalMethodes;
+import smartcity.begrouped.utils.MyApplication;
 
 /**
  * Created by a on 27/04/2015.
  */
 public class POIManager {
 
-    public static LinkedList<POI> createPoiForTest() {
-        LinkedList<POI> lesPOI = new LinkedList<POI>();
-        for (int i = 0; i < 5; i++) {
-            POI poi = new POI("type : " + i, "type detail : " + i,
-                    "name " + i, "address " + i, "phone " + i);
-            lesPOI.add(poi);
+
+
+
+    public static LinkedList<POI> searchPOIByName(String searchName){
+        JSONArray poiList;
+        LinkedList<POI> listOfPOI=new LinkedList<POI>();
+        String jsonFileUrl = GlobalMethodes.getFromUrl(AllUrls.SEARCH_POI_BY_NAME+searchName+"/" + MyApplication.myIdentity.getUsername()+"/"+MyApplication.myIdentity.getPassword());
+        Log.v("Jsonfile : ", jsonFileUrl);
+
+        //Json file parser
+        if (jsonFileUrl != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonFileUrl);
+
+                // Getting JSON Array node
+                poiList= jsonObj.getJSONArray(Constants.POINTS);
+
+                // looping through All members
+                for (int i = 0; i < poiList.length(); i++) {
+                    JSONObject jsonObject = poiList.getJSONObject(i);
+
+                    String type= (String) jsonObject.get(Constants.TYPE);
+                    String typeDetail= (String) jsonObject.get(Constants.TYPE_DETAIL);
+                    String name= (String) jsonObject.get(Constants.NAME);
+                    String latitude= (String) jsonObject.get(Constants.LATITUDE);
+                    String longitude= (String) jsonObject.get(Constants.LONGITUDE);
+
+                    String address= (String) jsonObject.get(Constants.ADDRESS);
+                    String phone= (String) jsonObject.get(Constants.PHONE);
+                    String email= (String) jsonObject.get(Constants.EMAIL);
+                    String website= (String) jsonObject.get(Constants.WEBSITE);
+                    listOfPOI.add(new POI(type, typeDetail, name, address, phone, email, website, new Location(Double.parseDouble(latitude), Double.parseDouble(longitude))));
+                }
+                return listOfPOI;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e("ServiceHandler", "Couldn't get any data from the url");
         }
-        return lesPOI;
+        return null;
     }
+
+
+    public static LinkedList<POI> getDayProgramOfGroup(Date date,String groupName){
+
+        JSONArray poiList;
+        LinkedList<POI> listOfPOI=new LinkedList<POI>();
+        String jsonFileUrl = GlobalMethodes.getFromUrl(AllUrls.GET_PROGRAM+groupName+"/"+date.toString()+"/" + MyApplication.myIdentity.getUsername()+"/"+MyApplication.myIdentity.getPassword());
+        Log.v("Jsonfile : ", jsonFileUrl);
+
+        //Json file parser
+        if (jsonFileUrl != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonFileUrl);
+
+                // Getting JSON Array node
+                poiList= jsonObj.getJSONArray(Constants.PROGRAM);
+
+                // looping through All members
+                for (int i = 0; i < poiList.length(); i++) {
+                    JSONObject jsonObject = poiList.getJSONObject(i);
+
+                    String type= (String) jsonObject.get(Constants.TYPE);
+                    String typeDetail= (String) jsonObject.get(Constants.TYPE_DETAIL);
+                    String name= (String) jsonObject.get(Constants.NAME);
+                    String latitude= (String) jsonObject.get(Constants.LATITUDE);
+                    String longitude= (String) jsonObject.get(Constants.LONGITUDE);
+                    String address= (String) jsonObject.get(Constants.ADDRESS);
+                    String phone= (String) jsonObject.get(Constants.PHONE);
+                    String email= (String) jsonObject.get(Constants.EMAIL);
+                    String website= (String) jsonObject.get(Constants.WEBSITE);
+                    String dateSTR=(String) jsonObject.get(Constants.DATE);
+                    String tempsSTR=(String) jsonObject.get(Constants.TEMPS);
+                    POI poi=new POI(type,typeDetail,name,address,phone,email,website, new Location(Double.parseDouble(latitude), Double.parseDouble(longitude)));
+                    poi.setDateOfVisite(Date.dateFromLittleString(dateSTR));
+                    poi.setTempsOfVisite(Temps.tempsFromLittleString(tempsSTR));
+                    listOfPOI.add(poi);
+
+                }
+                return listOfPOI;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e("ServiceHandler", "Couldn't get any data from the url");
+        }
+        return null;
+    }
+    public static LinkedList<POI> getProgramOfGroup(String groupName){
+
+        JSONArray poiList;
+        LinkedList<POI> listOfPOI=new LinkedList<POI>();
+        String jsonFileUrl = GlobalMethodes.getFromUrl(AllUrls.GET_PROGRAM+groupName+"/" + MyApplication.myIdentity.getUsername()+"/"+MyApplication.myIdentity.getPassword());
+        Log.v("Jsonfile : ", jsonFileUrl);
+
+        //Json file parser
+        if (jsonFileUrl != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonFileUrl);
+
+                // Getting JSON Array node
+                poiList= jsonObj.getJSONArray(Constants.PROGRAM);
+
+                // looping through All members
+                for (int i = 0; i < poiList.length(); i++) {
+                    JSONObject jsonObject = poiList.getJSONObject(i);
+
+                    String type= (String) jsonObject.get(Constants.TYPE);
+                    String typeDetail= (String) jsonObject.get(Constants.TYPE_DETAIL);
+                    String name= (String) jsonObject.get(Constants.NAME);
+                    String latitude= (String) jsonObject.get(Constants.LATITUDE);
+                    String longitude= (String) jsonObject.get(Constants.LONGITUDE);
+                    String address= (String) jsonObject.get(Constants.ADDRESS);
+                    String phone= (String) jsonObject.get(Constants.PHONE);
+                    String email= (String) jsonObject.get(Constants.EMAIL);
+                    String website= (String) jsonObject.get(Constants.WEBSITE);
+                    String dateSTR=(String) jsonObject.get(Constants.DATE);
+                    String tempsSTR=(String) jsonObject.get(Constants.TEMPS);
+                    POI poi=new POI(type,typeDetail,name,address,phone,email,website, new Location(Double.parseDouble(latitude), Double.parseDouble(longitude)));
+                    poi.setDateOfVisite(Date.dateFromLittleString(dateSTR));
+                    poi.setTempsOfVisite(Temps.tempsFromLittleString(tempsSTR));
+                    listOfPOI.add(poi);
+
+                }
+                return listOfPOI;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e("ServiceHandler", "Couldn't get any data from the url");
+        }
+        return null;
+    }
+    public static void initDayGroupProgram(Date date,String groupName){
+        String jsonFileUrl = GlobalMethodes.getFromUrl(AllUrls.DELETE_GROUP+groupName+"/"+date.toString()+"/" + MyApplication.myIdentity.getUsername()+"/"+MyApplication.myIdentity.getPassword());
+    }
+    public static void saveDayGroupProgram(Date date, String grouName, LinkedList<POI> dayProgram){
+
+    }
+
+
+
+
+
+
 }
