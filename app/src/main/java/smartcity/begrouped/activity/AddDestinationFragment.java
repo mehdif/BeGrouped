7e5c08a7aa2e2e3c364b6a2cbc0108af5b6a0664
hakkan <yanour3ini>
2015-05-02 14:39:32 +0200

    date picker

--- app/src/main/java/smartcity/begrouped/activity/AddDestinationFragment.java
+++ app/src/main/java/smartcity/begrouped/activity/AddDestinationFragment.java
@@ -4,14 +4,21 @@ package smartcity.begrouped.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import smartcity.begrouped.R;
 
 
 public class AddDestinationFragment extends Fragment {
+
+    private EditText search_query;
 
     public AddDestinationFragment() {
         // Required empty public constructor
@@ -27,7 +34,22 @@ public class AddDestinationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_add_destination, container, false);
+        search_query = (EditText) rootView.findViewById(R.id.search_input);
 
+        search_query.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                    Toast.makeText(getActivity(), "Looking for : "+search_query.getText(), Toast.LENGTH_LONG).show();
+
+                    // Anes : Call your search function here : performSearch();
+
+                    return true;
+                }
+                return false;
+            }
+        });
 
         // Inflate the layout for this fragment
         return rootView;
