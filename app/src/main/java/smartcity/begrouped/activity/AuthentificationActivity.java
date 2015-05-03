ce94ebc056e1cd1d439cbffe3d582697164e6995
mehdif <dif.mehdi@ymail.com>
2015-05-03 16:43:11 +0200

    notification

--- app/src/main/java/smartcity/begrouped/activity/AuthentificationActivity.java
+++ app/src/main/java/smartcity/begrouped/activity/AuthentificationActivity.java
@@ -1,6 +1,9 @@
 package smartcity.begrouped.activity;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -80,7 +83,6 @@ public class AuthentificationActivity extends ActionBarActivity {
                     startActivity(i);
                     overridePendingTransition(R.anim.right_in, R.anim.left_out);
 
-
                 }
             });
         }
@@ -145,4 +147,6 @@ public class AuthentificationActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
--- app/src/main/java/smartcity/begrouped/controllers/ReceiverNearestPoint.java
+++ app/src/main/java/smartcity/begrouped/controllers/ReceiverNearestPoint.java
@@ -1,6 +1,9 @@
 package smartcity.begrouped.controllers;
 
+import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -8,7 +11,12 @@ import android.content.Intent;
 import android.os.SystemClock;
 import android.util.Log;
 
+import java.util.LinkedList;
+
+import smartcity.begrouped.R;
+import smartcity.begrouped.activity.AuthentificationActivity;
 import smartcity.begrouped.activity.MapsActivity;
+import smartcity.begrouped.model.POI;
 import smartcity.begrouped.utils.MyApplication;
 
 /**
@@ -28,10 +36,8 @@ public class ReceiverNearestPoint extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (MyApplication.myIdentity!=null) {
+        if (MyApplication.myIdentity != null) {
             Log.i("receiver", "receiver de nearest point");
-            // Wa rrougi dir traitement te3ek hna
-
 
             pi = PendingIntent.getBroadcast(context, 0, new Intent(
                     "com.authorwjf.nearestpoint"), 0);
@@ -39,6 +45,30 @@ public class ReceiverNearestPoint extends BroadcastReceiver {
             am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                     SystemClock.elapsedRealtime() + FIVE_SECONDS, pi);
 
+            // Prepare intent which is triggered if the
+            // notification is selected
+            Intent intent1 = new Intent(context, AuthentificationActivity.class);
+            PendingIntent pIntent = PendingIntent.getActivity(context, 0, intent1, 0);
+
+            // Build notification
+            // Actions are just fake
+            if (MyApplication.myPosition != null) {
+
+                LinkedList<POI> listPOI = POIManager.getNearestPoiByTask(MyApplication.myPosition.getLatitude(), MyApplication.myPosition.getLongitude());
+
+                if (listPOI != null) {
+                    Notification noti = new Notification.Builder(context)
+                            .setContentTitle("You are near " + listPOI.get(0).getName())
+                            .setContentText(listPOI.get(0).getType()).setSmallIcon(R.drawable.monument)
+                            .setContentIntent(pIntent).build();
+                    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Activity.NOTIFICATION_SERVICE);
+                    // hide the notification after its selected
+                    noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+                    notificationManager.notify(0, noti);
+                }
+
+            }
         }
     }
 }
