dd5deab80a236a89f0be23dcf48943a9614a566c
Anes <bendimeradanes@gmail.com>
2015-05-03 15:30:12 +0200

    Commited

--- app/src/main/java/smartcity/begrouped/activity/ProgramFragment.java
+++ app/src/main/java/smartcity/begrouped/activity/ProgramFragment.java
@@ -100,7 +100,7 @@ public class ProgramFragment extends Fragment {
         //Array of data to fill in the list
         listItem1 = new ArrayList<HashMap<String, String>>();
         //LinkedList<Group> mygroups= GroupManager.getGroups();
-        LinkedList<POI> myPOIs= POIManager.getDayProgramOfGroupByTask(MyApplication.dateOfCurrentProgram,MyApplication.currentGroup.getName());
+        LinkedList<POI> myPOIs= POIManager.sortPOIByTime(POIManager.getDayProgramOfGroupByTask(MyApplication.dateOfCurrentProgram,MyApplication.currentGroup.getName()));
         if (myPOIs==null) myPOIs=new LinkedList<>();
         for(int i=0;i<myPOIs.size();i++) {
             POI poi= myPOIs.get(i);
