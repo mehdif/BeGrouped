8fa2f07722aca2a026f4ff3000412153ec78b330
maha <maha.benabbou93@gmail.com>
2015-05-05 10:52:02 +0200

    Commited

--- app/src/main/java/smartcity/begrouped/utils/MessageService.java
+++ app/src/main/java/smartcity/begrouped/utils/MessageService.java
@@ -4,14 +4,11 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-import com.parse.Parse;
-import com.parse.ParseException;
+
 import com.parse.ParseInstallation;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 import com.sinch.android.rtc.ClientRegistration;
 import com.sinch.android.rtc.Sinch;
 import com.sinch.android.rtc.SinchClient;
